ac68db007e6a563096f5f3235b40e05021a0fecd
linedev06 <vitorafelismino@gmail.com>
2023-04-30 14:27:24 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string temperatura, rotacao, pressao;
+Console.WriteLine("--- Luz De Advertencia ---\n");
+
+Console.Write("Sensor de temperatura: ");
+temperatura = Console.ReadLine()!;
+
+Console.Write("Sensor de pressao: ");
+pressao = Console.ReadLine()!;
+
+Console.Write("Sensor de rotação: ");
+rotacao = Console.ReadLine()!;
+
+if (pressao == "A" && temperatura == "A" && rotacao == "D") 
+{
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("A luz de advertencia esta ativada");
+
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Luz de advertencia desativada");
+}
+Console.ResetColor();
